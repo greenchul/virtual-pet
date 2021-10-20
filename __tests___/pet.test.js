dada096b87619fd1c9162b909b8d41fa8b5d16ed
Rachel Sanham <green_chul@hotmail.com>
2021-10-20 17:56:11 +0100

    Added growUp method and tests

--- __tests___/pet.test.js
+++ __tests___/pet.test.js
@@ -11,3 +11,13 @@ describe("constructor", ()=>{
         expect(pet.name).toBe("Steve")
     })
 })
+
+describe("pet methods", ()=>{
+    test("Pet should age by 1 when growUp method called", ()=>{
+        const pet = new Pet("Steve")
+        expect(pet.age).toBe(0)
+        pet.growUp()
+        expect(pet.age).toBe(1)
+    })
+    
+})
--- src/pet.js
+++ src/pet.js
@@ -1,6 +1,10 @@
 class Pet{
     constructor(name){
         this.name = name
+        this.age = 0
+    }
+    growUp(){
+        this.age += 1
     }
 }
 
